a60a989e1fd742cf9e3845aba44f79525e835f15
Biborka Bartus <biborkabartus@gmail.com>
2026-08-11 10:19:17 +0200

    Configured database connection

---
+++ backend/src/main/java/com/cluj1/eventapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cluj1.eventapp.repository;
+
+import com.cluj1.eventapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
