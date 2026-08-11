15c3554d71bb0202c4b8df153de5798247807978
Biborka Bartus <biborkabartus@gmail.com>
2026-08-11 08:22:43 +0200

    Project initialization

---
+++ backend/src/test/java/com/cluj1/eventapp/EventappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cluj1.eventapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
