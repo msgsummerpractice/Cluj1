bb833e7c2585bd4eea29b4671792ece244b5cf79
biancaholobica <bianca.holobica@stud.ubbcluj.ro>
2026-08-11 15:50:16 +0300

    Add @ToString.Exclude where needed

--- backend/src/main/java/com/cluj1/eventapp/model/User.java
+++ backend/src/main/java/com/cluj1/eventapp/model/User.java
@@ -10,41 +10,48 @@ import com.cluj1.eventapp.model.enums.Role;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
- 
+
 @Entity
 @Table(name = "users")
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
- 
+
     @Column(nullable = false, unique = true)
     @Pattern(
         regexp = "^[a-zA-Z0-9]+\\.[a-zA-Z0-9]+@msg\\.[a-zA-Z0-9]+$",
         message = "Invalid email format"
-)
+    )
     private String email;
- 
+
+    @ToString.Exclude
     @Column(name = "password_hash", nullable = false)
     private String passwordHash;
- 
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 50)
     @Builder.Default
     private Role role = Role.PARTICIPANT;
- 
+
     @Column(name = "is_active", nullable = false)
     @Builder.Default
     private Boolean isActive = true;
- 
+
     @CreationTimestamp
     @Column(name = "created_at", updatable = false)
     private OffsetDateTime createdAt;
- 
+
     @UpdateTimestamp
     @Column(name = "updated_at")
     private OffsetDateTime updatedAt;
+
+    @ToString.Exclude
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private UserDetails userDetails;
 }
