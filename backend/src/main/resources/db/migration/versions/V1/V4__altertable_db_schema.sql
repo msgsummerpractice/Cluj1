9472bed2b482bd8dfdab9c673e3455f165e283c1
biancaholobica <bianca.holobica@stud.ubbcluj.ro>
2026-08-17 20:35:44 +0300

    fix database constraint for food_provider

--- backend/src/main/resources/db/migration/versions/V1/V4__altertable_db_schema.sql
+++ backend/src/main/resources/db/migration/versions/V1/V4__altertable_db_schema.sql
@@ -1,8 +1,5 @@
--- Setează valoarea implicită (default) pe false pentru coloana food_provided
 ALTER TABLE event_details ALTER COLUMN food_provided SET DEFAULT false;
 
--- Actualizează toate înregistrările existente care au în acest moment valoarea NULL
 UPDATE event_details SET food_provided = false WHERE food_provided IS NULL;
 
--- Scoate constrângerea de NOT NULL ca să permită valori implicite fără erori
 ALTER TABLE event_details ALTER COLUMN food_provided DROP NOT NULL;
