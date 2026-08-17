26857d7d3561fd502387df13647250e8aa89085f
biancaholobica <bianca.holobica@stud.ubbcluj.ro>
2026-08-17 12:39:13 +0300

    Add backend pattern validation

--- backend/src/main/java/com/cluj1/eventapp/dto/ForgotPasswordRequest.java
+++ backend/src/main/java/com/cluj1/eventapp/dto/ForgotPasswordRequest.java
@@ -1,6 +1,7 @@
 package com.cluj1.eventapp.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,9 @@ import lombok.NoArgsConstructor;
 public class ForgotPasswordRequest {
     @NotBlank(message = "Email is required.")
     @Email(message = "Invalid email format.")
+    @Pattern(
+        regexp = "^[a-zA-Z0-9]+\\.[a-zA-Z0-9]+@msg\\.group$",
+        message = "Invalid email format"
+    )
     private String email;
 }
