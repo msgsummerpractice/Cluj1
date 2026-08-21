207e8c1dbfce58f9008b4cc65a1f4ebf273f4b05
Biborka Bartus <biborkabartus@gmail.com>
2026-08-21 10:41:35 +0200

    fix: event register driver modification

--- backend/src/main/java/com/cluj1/eventapp/service/EventService.java
+++ backend/src/main/java/com/cluj1/eventapp/service/EventService.java
@@ -451,7 +451,7 @@ public class EventService {
             throw new InvalidEventOperationException("GDPR consent is required for this event type.");
         }
 
-        if (event.getType() == EventType.INTERNAL) {
+        if (event.getType() != EventType.EXTERNAL) {
             validateInternalRegistration(dto);
         }
 
@@ -466,7 +466,7 @@ public class EventService {
                 .accommodationDays(dto.getAccommodationDays())
                 .build();
 
-        if (event.getType() == EventType.INTERNAL && Boolean.TRUE.equals(dto.getTransportationNeeded())) {
+        if (event.getType() != EventType.EXTERNAL && Boolean.TRUE.equals(dto.getTransportationNeeded())) {
             TransportationDetails transportDetails = TransportationDetails.builder()
                     .registration(registration)
                     .driverName(dto.getDriverName())
@@ -591,7 +591,7 @@ public class EventService {
             return null;
         }
 
-        if (event.getType() == EventType.INTERNAL) {
+        if (event.getType() != EventType.EXTERNAL) {
             validateInternalRegistration(eventRegistrationDto);
         }
 
@@ -642,7 +642,7 @@ public class EventService {
     }
 
     private void updateTransportationDetails(Registration registration, Event event, EventRegistrationDto dto) {
-        if (event.getType() == EventType.INTERNAL && Boolean.TRUE.equals(dto.getTransportationNeeded())) {
+        if (event.getType() != EventType.EXTERNAL && Boolean.TRUE.equals(dto.getTransportationNeeded())) {
             TransportationDetails transportDetails = registration.getTransportationDetails();
 
             if (transportDetails == null) {
